0fd5f61b4ce6024438ec79a81d7015c8c21813e2
mohamed.elshawaf <mohamed.elshawaf.1@gmail.com>
2023-10-07 10:09:04 +0300

    Initial Commit

---
+++ MonyManagment/MonyManagmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonyManagmentApp.swift
+//  MonyManagment
+//
+//  Created by Mohamed Elshawaf on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MonyManagmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
